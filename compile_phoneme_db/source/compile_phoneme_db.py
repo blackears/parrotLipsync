eb23060c62b8408ba49f4725e49aa04cd398fad1
Mark McKay <mark@kitfox.com>
2024-06-20 08:42:26 -0400

    Now downloading Libravox website data.

--- compile_phoneme_db/source/compile_phoneme_db.py
+++ compile_phoneme_db/source/compile_phoneme_db.py
@@ -11,9 +11,10 @@ libravox_pages = [
     "https://librivox.org/christmas-short-works-collection-2022-by-various/",
 ]
 
-source_audio_urls = []
 
-for url in libravox_pages:
+def download_libravox_webpage(url):
+    source_audio_urls = []
+
     reqs = requests.get(url)
     soup = BeautifulSoup(reqs.text, 'html.parser')
 
@@ -21,13 +22,20 @@ for url in libravox_pages:
     for link in soup.find_all('a'):
         page_link = link.get('href')
         if page_link.endswith(".mp3"):
-            source_audio_urls.append(page_link)
             
             url_parts = urlparse(page_link)
             file_path = completeName = os.path.join(cache_dir, url_parts.path[1:])
             file_path = os.path.abspath(file_path)
             os.makedirs(os.path.dirname(file_path), exist_ok=True)
-            print("--" + file_path)
+            
+            if not("_64kb" in file_path):
+                continue
+
+            print("Downloading to: " + file_path)
+            source_audio_urls.append(page_link)
+            
+            if os.path.exists(file_path):
+                continue
             
             response = requests.get(page_link)
 
@@ -38,9 +46,12 @@ for url in libravox_pages:
             else:
                 print('Failed to download file')
                 
-            break
+#    print(source_audio_urls)
+    return source_audio_urls
 
-print(source_audio_urls)
+for url in libravox_pages:
+    download_libravox_webpage(url)
+
 
 
 

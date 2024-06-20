3ce6b5460746da194942a0702a4c030ca10097e7
Mark McKay <mark@kitfox.com>
2024-06-20 08:25:45 -0400

    Investigating creating custom phoneme database.

---
+++ compile_phoneme_db/source/compile_phoneme_db.py
@@ -0,0 +1,51 @@
+import requests
+from bs4 import BeautifulSoup
+import requests 
+from urllib.parse import urlparse
+import os
+import os.path
+
+cache_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../cache")
+
+libravox_pages = [
+    "https://librivox.org/christmas-short-works-collection-2022-by-various/",
+]
+
+source_audio_urls = []
+
+for url in libravox_pages:
+    reqs = requests.get(url)
+    soup = BeautifulSoup(reqs.text, 'html.parser')
+
+    urls = []
+    for link in soup.find_all('a'):
+        page_link = link.get('href')
+        if page_link.endswith(".mp3"):
+            source_audio_urls.append(page_link)
+            
+            url_parts = urlparse(page_link)
+            file_path = completeName = os.path.join(cache_dir, url_parts.path[1:])
+            file_path = os.path.abspath(file_path)
+            os.makedirs(os.path.dirname(file_path), exist_ok=True)
+            print("--" + file_path)
+            
+            response = requests.get(page_link)
+
+            if response.status_code == 200:
+                with open(file_path, 'wb') as file:
+                    file.write(response.content)
+                print('File downloaded successfully')
+            else:
+                print('Failed to download file')
+                
+            break
+
+print(source_audio_urls)
+
+
+
+# sources = [
+#     "https://www.archive.org/download/christmasshortworks2022_2212_librivox/cswc2022_animalschristmastree_peters_128kb.mp3",
+#     "https://www.archive.org/download/christmasshortworks2022_2212_librivox/cswc2022_anniewilliesprayer_snow_128kb.mp3"    
+    
+# ]
